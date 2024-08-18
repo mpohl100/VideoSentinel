ff9764247047c2242533a5283a70b1c09600ca6b
Michael Pohl <mpohl100@googlemail.com>
2024-08-18 18:15:12 +0200

    change name of include directories to video-sentinel

--- app/src/main/cpp/update-video-sentinel-lib.sh
+++ app/src/main/cpp/update-video-sentinel-lib.sh
@@ -2,3 +2,6 @@ rm -rf video-sentinel
 git clone git@github.com:mpohl100/video-sentinel-cpp.git temp-clone
 cp -r temp-clone/src/ video-sentinel/
 rm -rf temp-clone
+cd video-sentinel
+find . -type f -name "CMakeLists.txt" -exec sed -i 's|\${CMAKE_SRC_DIR}/src|\${CMAKE_SRC_DIR}/video-sentinel|g' {} +
+cd ..
