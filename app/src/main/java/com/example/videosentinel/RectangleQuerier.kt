abe83153d142673bb1be6b99239a533e114b8e48
Michael Pohl <mpohl100@googlemail.com>
2024-09-02 19:08:52 +0200

    correct binding for C++ methods. got project to build

--- app/src/main/java/com/example/videosentinel/RectangleQuerier.kt
+++ app/src/main/java/com/example/videosentinel/RectangleQuerier.kt
@@ -6,7 +6,21 @@ import java.util.concurrent.Executors
 class RectangleQuerier(private val rectangleOverlayView: RectangleOverlayView) {
 
     fun computeRectangles(){
-        // TODO get rectangles from C++ and set them in the rectangleOverlay
+        // get rectangles from C++ and set them in the rectangleOverlay
+        if(arenewrectanglesavailable()){
+            val rectangles = getrectangles()
+            rectangleOverlayView.updateRectangles(rectangles)
+        }
     }
 
+
+    // JNI methods
+    external fun arenewrectanglesavailable(): Boolean
+    external fun getrectangles(): Array<Rectangle>
+
+    companion object {
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
 }
